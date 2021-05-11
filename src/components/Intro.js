446bbe995589ce105c92e668f4a84b08c397698e
Sahidul Arif <sahidularif1@gmail.com>
2021-05-12 04:41:08 +0600

    updated

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,23 @@
-import React from "react"
-
+import React, { Component } from 'react'
+import Typical from 'react-typical'
 import { Container, Title, Button } from "./common"
 
 import "./intro.css"
 
 const Intro = () => {
+  const steps = [
+    'Hello 👋!!! I am', 1000,
+  ];
   return (
     <div className="intro-area">
       <Title title="Follow Me" social height="320px" />
       <Container>
         <div className="intro">
           <div className="intro-text">
-            <p>Hello! I am</p>
+            <p><Typical wrapper="span" steps={steps} loop={1} className={'caca'} /></p>
             <h1>Sahidul Arif</h1>
             <h3>Front-End Developer</h3><br/>
+            
             <div className="action">
               <Button
                 link="https://github.com/sahidularif/arif.me/blob/main/src/data/file/MyResume.pdf"
